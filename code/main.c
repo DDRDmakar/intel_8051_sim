586a3b5ca8b48807b1440290ed1415ee66120a9f
DDRDmakar <DDRDmakar@localhost.localdomain>
2018-04-24 23:32:03 +0300

    Deleted binaries  Changes to be committed: 	new file:   .gitignore 	deleted:    build/main.o 	deleted:    build/sim8051 	modified:   code/main.c

--- code/main.c
+++ code/main.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
 
 int main(int argc, char** argv)
@@ -6,6 +6,36 @@ int main(int argc, char** argv)
 	for (int i = 0; i < argc; ++i)
 	{
 		printf("%s\n", argv[i]);
+	}
+	
+	for (int i = 0; i < argc; ++i)
+	{
+		if (strlen(argv[i]) >= 2 && argv[i][0] == '-')
+		{
+			if (argv[i][1] == '-')
+			{
+				// Полное написание имени флага
+			}
+			
+			for (int j = 1; j < strlen(argv[i]); ++j)
+			{
+				printf("Processing flag %c\n", argv[i][j]);
+				
+				switch (argv[i][j])
+				{
+					case 'h': {}
+					case 'o': {}
+					case 'c': {}
+					case 'v': {}
+					case 's': {}
+					case 'm': {}
+					case 'b': {}
+					case 'z': {}
+					
+					default: {}
+				}
+			}
+		}
 	}
 	
 	return 0;
